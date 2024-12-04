424bded2e46b008c5b423b7a309bc4c2f26afb47
Noel Schneider <noelwschneider@gmail.com>
2024-12-04 17:38:23 -0600

    add flask-migrations to project

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
 jwt = JWTManager()
+migrate = Migrate()
 
 
 def create_app():
@@ -13,6 +15,7 @@ def create_app():
     CORS(app)
     app.config.from_object('config.Config')
     db.init_app(app)
+    migrate.init_app(app, db)
     jwt.init_app(app)
 
     with app.app_context():
