b7592d15f043acdf4c3b7b6865647eed953042ca
Noel Schneider <noelwschneider@gmail.com>
2025-01-16 13:28:48 -0600

    add catalogs api
    minor cleanup in _api.py as well

--- app/api/_api.py
+++ app/api/_api.py
@@ -2,11 +2,15 @@ from flask import Blueprint
 
 
 def register_apis(app):
-    from .auth import auth
-    from .spotify import spotify
-
     api = Blueprint('api', __name__, url_prefix='/api')
+
+    from .auth import auth
     api.register_blueprint(auth, url_prefix='/auth')
+
+    from .catalogs import catalogs
+    api.register_blueprint(catalogs, url_prefix="/catalogs")
+
+    from .spotify import spotify
     api.register_blueprint(spotify, url_prefix='/spotify')
 
     app.register_blueprint(api)
